21964163ddf09370e25f33c30b8eb5a143fae37a
unknown <LSI@LSI-PC.(none)>
2012-06-02 05:27:33 +0900

    A simple uart communication AVR-MATLAB send and parse character. not array.

--- matlab/AVRtest/AVRpractice.c
+++ matlab/AVRtest/AVRpractice.c
@@ -20,8 +20,7 @@
 unsigned char send = '0';
 char buffer[BUFFER_LENGTH];
 
-static FILE myStream = FDEV_SETUP_STREAM(TxChar, RxChar, _FDEV_SETUP_RW);
-int a=0, b=0, c=0;
+char a;
 
 
 /******************************
@@ -58,6 +57,7 @@ unsigned char RxChar(void)
 }
 
 
+static FILE myStream = FDEV_SETUP_STREAM(TxChar, RxChar, _FDEV_SETUP_RW);
 /*******************************
 	main function
 *******************************/
@@ -67,10 +67,15 @@ int main(void)
 	InitINT();
 	
 	DDRC = 0xff;
+	PORTC = 0xff;
+	_delay_ms(50);
 	PORTC = 0x00;
 
 	sei();
-	while(1){PORTC = (a<<2)|(b<<1)|(c<<0);}
+	while(1){
+		if(a == '1')
+			PORTC = (1<<0);
+	}
 
 	return 0;
 }
@@ -81,14 +86,10 @@ int main(void)
 /**************************
 	interrupt function
 ***************************/
-SIGNAL(SIG_INTERRUPT7)
-{
-	fprintf(&myStream,"%d\n",1);
-}
 
-SIGNAL(SIG_INTERRUPT6)
+SIGNAL(SIG_UART0_RECV)
 {
-	fprintf(&myStream,"%d\n",2);
-	_delay_ms(1);
-	fscanf(&myStream, "%d %d %d\n", &a, &b, &c);
+	a = UDR0;
+//	fscanf(&myStream, "%s\n", buffer);
+	fprintf(&myStream, "%c\n", a);
 }
