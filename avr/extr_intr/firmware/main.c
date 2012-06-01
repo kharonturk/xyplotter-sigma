bbfc103e257a2c623c926531344e56aaf3898487
majecty <macbook@MAC-BOOKui-MacBook-Pro.local>
2012-06-01 18:04:53 +0900

    모터드라이버 테스트 하는 코드이고 모터드라이버 테스트도 통과했다.

--- avr/extr_intr/firmware/main.c
+++ avr/extr_intr/firmware/main.c
@@ -10,13 +10,22 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 
+typedef struct motor
+{
+    unsigned char dir;
+    unsigned char speed; // 1 is slowest 254 is fastest
+} Motor;
+
 void port_init();
 void ext_intr_init();
 void uart_init();
+void timer_init();
+void motor_init();
 void uart_transmit(unsigned char data);
 int putChar(char data, FILE* f);
 
 volatile uint64_t count = 0;
+volatile Motor motorA;
 
 ISR(INT4_vect)
 {
@@ -24,17 +33,41 @@ ISR(INT4_vect)
     PORTA = count;
 }
 
+ISR(TIMER0_COMP_vect)
+{
+    PORTC ^= (1<<0);
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    motorA.speed++;
+    OCR0 = motorA.speed;
+    if( motorA.dir)
+    {
+        PORTC |= (1<<0);
+        PORTC &= ~(1<<2);
+    }
+    else 
+    {
+        PORTC &= ~(1<<0);
+        PORTC |= (1<<2);
+    }
+}
+
 int main(void)
 {
     port_init();
     ext_intr_init();
     uart_init();
+    timer_init();
+    motor_init();
     /* insert your hardware initialization here */
     sei();
 
     for(;;)
     {
         _delay_ms(100);
+//        OCR0+=10;
         printf("%d is interrupt count \r\n", count);
         /* insert your main loop code here */
     }
@@ -47,7 +80,9 @@ void port_init()
     DDRE |= (1<<PE1);  // for uart tx
     DDRE &= ~(1<<PE0); // for uart rx
     DDRA = 0xff;       // for led;
+    DDRC = 0xff;       // for motor control
     PORTA = 0;
+    PORTC = 0;
 }
 
 void ext_intr_init()
@@ -61,6 +96,19 @@ void uart_init()
     UCSR1B |= (1<<TXEN);
     UBRR1L = 8;
     fdevopen(putChar,0);
+}
+
+void timer_init()
+{
+    TCCR0 |= (1<<CS02) | (1<<CS01) | (1<<CS00); //clk/1024
+    OCR0 = 0;
+    TIMSK |= (1<<OCIE0) | (1<<TOIE0);
+}
+
+void motor_init()
+{
+    motorA.dir   = 0;
+    motorA.speed = 1;
 }
 
 int putChar(char data, FILE* f)
