88566a4e442c9ac4e4e2f0f589043124918eb13c
majecty <macbook@MAC-BOOKui-MacBook-Pro.local>
2012-06-14 19:25:29 +0900

    불필요한 것 지웠습니다.

--- avr/using_step/firmware/main.c
+++ avr/using_step/firmware/main.c
@@ -10,14 +10,6 @@
 #include <stdint.h>
 #include "uart.h"
 #include "motor.h"
-//#define R (1<<6)
-//#define B (1<<4)
-//#define Y (1<<2)
-//#define W (1<<0)
-
-//#define G ~(1<<4)
-//#define R ~(1<<2)
-//#define D ~(1<<0)
 
 
 volatile uint64_t count = 0;
@@ -33,16 +25,6 @@ int main(void)
     sei();
 
     for(;;){
-//        PORTA = B|D;
-//        _delay_ms(10);
-//        PORTA = D|R;
-//        _delay_ms(10);
-//        PORTA = R|G;
-//        _delay_ms(10);
-//        PORTA = G|B;
-//        _delay_ms(10);
-//
-//        _delay_ms(100);
         while(stop) PORTA=0;
         if(dir)
         {
@@ -53,7 +35,6 @@ int main(void)
             cycle_backward(100);
         }
         count += 1;
-        /* insert your main loop code here */
     }
     return 0;   /* never reached */
 }
